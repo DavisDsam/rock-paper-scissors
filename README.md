6b69d737f17925624fece82da13facc90dd373ff
DavisDsam <bernavil35@gmail.com>
2022-12-09 12:15:36 -0500

    created function for computer selection, began function for player selection, added comments and notes.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # rock-paper-scissors
 Rock paper scissors assignment on TOP
+
+
+//create a function called getComputerChoice that chooses between three parameters, "rock", "paper", "scissors". 
+
+1: create variables for each parameter
+2: create a variable that the computer's choice will be stored in
+3: assign each choice to a number, "rock = 1, paper = 2, scissors = 3"
+
+function getChoice() {
+    let randomChoice = Math.floor(Math.random() * 3);   //decided to use an if else statement to practice that skill as well.//
+
+    if (randomChoice == 0) {                           // if computer chooses 0, then return rock, if 1, paper, if neither, return scissors. This still works because the only other option is 2.//
+        return "rock";
+    } else if (randomChoice == 1) {
+        return "paper"
+    } else { 
+        return "scissors"
+    }
+
+    
+}
+4
+5
+5
+6
+7
+
--- rps.js
+++ rps.js
@@ -1,2 +1,26 @@
 console.log("testing")
-alert("waddup")
+
+function getOpponentChoice() {
+    let randomNum = Math.floor(Math.random() * 3); //this calculates the computer's choice and stores it in the randomNum value//
+    switch (randomNum) {    //switch statement assigning each option (rock, paper, or scissors) to a number between one and three. 
+        case 0: 
+        return "rock";
+        break;
+        case 1: 
+        return "paper";
+        break;
+        case 2: 
+        return "scissors";
+        break;
+        default:
+        return"something went wrong"; //switch statements will always need a default to fall back on incase any of the conditions are not met
+        
+    }
+
+}
+
+let computerSelection 
+
+//next I need to creat a function called playRound that takes two parameters, playerSelection and computerSelection//
+//I will start with the prompt portion of the function that will ask the player to pick 'rock, paper, or scissors'. 
+    //The prompt will ask the question and store the answer in the playerSelection box//
